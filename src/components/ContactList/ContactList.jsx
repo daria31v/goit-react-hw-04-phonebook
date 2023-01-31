9b854e907f836de5250fbf8ea99655564fb64af5
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-01-31 14:17:24 +0100

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,11 +1,11 @@
 import PropTypes from 'prop-types';
 import {List, Contact} from './ContactList.styled'
 import { ContactItems } from '../ContactItems/ContactItems';
-import { useState } from 'react';
+// import { useState } from 'react';
 import {GiRotaryPhone} from 'react-icons/gi'
 
 export const ContactList = ({ items, onDelete }) => {
-  const [contacts, setContacts] = useState([]);
+  // const [contacts, setContacts] = useState([]);
    return (
       <List>
         {items.map(item => (
