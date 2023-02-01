827815fdfa8eec09948e35d9c47737177a9de71d
Daria Vdovichenko <darya.vdovichenko86@gmail.com>
2023-02-01 08:43:23 +0100

    fix  addContact

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,4 +1,3 @@
-
 import PropTypes from 'prop-types';
 import {
   AddContactBtn,
@@ -8,14 +7,6 @@ import {
 } from './ContactForm.styled';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { nanoid } from 'nanoid';
-
-//  import * as yup from 'yup';
-//  import "yup-phone";
-
-// const schema = yup.object().shape({
-// name: yup.string().required(),
-// number: yup.string().phone().required(),
-// });
 
 
 const FormError = ({ name }) => {
@@ -27,23 +18,14 @@ const FormError = ({ name }) => {
   );
 };
 
-export const ContactForm = ( {onSubmit} ) => {
-  // const submitForm = (values, { resetForm }) => {
-  //    onSubmit(values.name, values.number);
-  //    resetForm();
-  //  };
-  // const submitForm = (values, { resetForm }) => {
-  //   const { name, number } = values;
-  //   const data = {name, number}
-  //   resetForm();
-  //   return addContact(data);
-  // };
-
-   const submitForm = (values, { resetForm }) => {
-    this.props.onSubmit(values.name, values.number);
-    this.setState( values.name, values.number );
-
+export const ContactForm = ({ onSubmit }) => {
+ 
+  const submitForm = (values, { resetForm }) => {
+    const { name, number } = values;
+    const dataContact = { name, number };
+    
     resetForm();
+    onSubmit(dataContact);
   };
 
   const nameInputId = nanoid();
@@ -56,7 +38,6 @@ export const ContactForm = ( {onSubmit} ) => {
         number: '',
       }}
       onSubmit={submitForm}
-      // validationSchema={schema}
     >
       <Form autoComplete="off">
         <FormAddContacts>
@@ -95,61 +76,6 @@ export const ContactForm = ( {onSubmit} ) => {
   );
 };
 
-// export class ContactForm extends Component {
-//   nameInputId = nanoid();
-//   telInputId = nanoid();
-
-//   submitForm = (values, { resetForm }) => {
-//     this.props.onSubmit(values.name, values.number);
-//     this.setState({ initialValues });
-
-//     resetForm();
-//   };
-
-// render() {
-//   return (
-//     <Formik
-//       initialValues={initialValues}
-//       onSubmit={this.submitForm}
-//       // validationSchema={schema}
-//     >
-//       <Form autoComplete="off">
-//         <FormAddContacts>
-//           <Label htmlFor={this.nameInputId}>
-//             Name
-//             <Field
-//               type="text"
-//               name="name"
-//               placeholder="Jacob Mercer"
-//               pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//               required
-//               id={this.nameInputId}
-//             />
-//             <FormError name="name" component="span" />
-//           </Label>
-
-//           <Label htmlFor={this.telInputId}>
-//             Number
-//             <Field
-//               type="tel"
-//               name="number"
-//               placeholder="+380000000000"
-//               pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//               required
-//               id={this.telInputId}
-//             />
-//             <FormError name="number" component="span" />
-//           </Label>
-//         </FormAddContacts>
-
-//         <AddContactBtn type="submit">Add contacts</AddContactBtn>
-//       </Form>
-//     </Formik>
-//   );
-// }
-// }
 
 ContactForm.propTypes = {
   value: PropTypes.shape({
